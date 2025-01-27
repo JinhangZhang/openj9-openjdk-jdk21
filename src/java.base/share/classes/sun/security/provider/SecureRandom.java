f8633569b1e9234d5dfbed9b12ea7171e99fba3f
JinhangZhang <Jinhang.Zhang@ibm.com>
2025-01-27 10:03:40 -0500

    Remove the strong reference

--- src/java.base/share/classes/sun/security/provider/SecureRandom.java
+++ src/java.base/share/classes/sun/security/provider/SecureRandom.java
@@ -276,6 +276,7 @@ implements java.io.Serializable {
         Arrays.fill(state, (byte) 0x00);
         Arrays.fill(remainder, (byte) 0x00);
         remCount = 0;
+        digest = null;
     }
     /*[ENDIF] CRIU_SUPPORT */
 
